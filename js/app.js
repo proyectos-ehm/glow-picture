ffe41a1a835070a2d454b710f5e4de883a8cfed5
laloehm <laloehm1@gmail.com>
2022-01-17 17:58:28 -0600

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+    wow = new WOW({
+      mobile: false
+    })
+    new WOW().init();
+
+
+(function ($) {
+    "use strict";
+    $('[data-icon="hamburguer"]').on("click", function () {
+        if (!$('[data-icon="hamburguer-bar"]').hasClass("bar-on")) {
+            $('[data-icon="hamburguer-bar"]').addClass("bar-on");
+        } else {
+            $('[data-icon="hamburguer-bar"]').removeClass("bar-on");
+        }
+    });
+})(jQuery);
+
+/* Smooth scrolling para anclas */
+$("a.smooth").on("click", function (e) {
+    e.preventDefault();
+    var $link = $(this);
+    var anchor = $link.attr("href");
+    $("html, body")
+        .stop()
+        .animate(
+            {
+                scrollTop: $(anchor).offset().top - 100 + "px",
+            },
+            1000
+        );
+});
